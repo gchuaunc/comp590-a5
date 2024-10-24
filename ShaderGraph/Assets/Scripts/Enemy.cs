1bab8971f2f7197c0dc4d0637f3e1c7724eb34da
Gabrian Chua <57601516+gabrianrchua@users.noreply.github.com>
2024-10-23 22:59:33 -0400

    feat: add basic enemy

--- ShaderGraph/Assets/Scripts/Enemy.cs
+++ ShaderGraph/Assets/Scripts/Enemy.cs
@@ -4,9 +4,32 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private NavMeshAgent agent;
+    [SerializeField] private float damage = 10f;
+    [SerializeField] private float damageRadius = 5f;
+    [SerializeField] private float damageCooldownSeconds = 1f;
+
+    private float cooldown = 0f;
 
     private void FixedUpdate()
     {
-        agent.SetDestination(Player.instance.transform.position);
+        Vector3 playerPosition = Player.instance.transform.position;
+        float distanceToPlayer = Vector3.Distance(playerPosition, transform.position);
+
+        if (distanceToPlayer > damageRadius / 1.25)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(playerPosition);
+        } else
+        {
+            agent.isStopped = true;
+        }
+
+        if (cooldown < 0.1f && distanceToPlayer < damageRadius)
+        {
+            Player.instance.Hurt(damage);
+            cooldown = damageCooldownSeconds;
+        }
+
+        cooldown -= Time.fixedDeltaTime;
     }
 }
--- ShaderGraph/Assets/Scripts/Player.cs
+++ ShaderGraph/Assets/Scripts/Player.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float maxHealth = 100f;
+
+    private float health;
+
     public static Player instance;
 
     private void Awake()
@@ -16,9 +20,29 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        health = maxHealth;
+    }
+
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Hurt(float amount)
+    {
+        health -= amount;
+        if (health < 0f)
+        {
+            Die();
+        }
+        Debug.Log("My health decreased by " + amount + " and now is " + health);
+    }
+
+    private void Die()
+    {
+        Debug.Log("I died!");
     }
 }
